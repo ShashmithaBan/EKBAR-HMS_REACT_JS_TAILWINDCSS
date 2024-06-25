36e129d4b2a65cd840afa22b4381c96119706f87
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-26 01:07:38 +0530

    implemented the booking form successfully

--- my-app/src/Components/Pages/AddBooking/CreateBooking.jsx
+++ my-app/src/Components/Pages/AddBooking/CreateBooking.jsx
@@ -1,7 +1,11 @@
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Button, Tooltip, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
-import { Button } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
 const initialValues = {
   fullName: '',
@@ -9,15 +13,18 @@ const initialValues = {
   numOfAdults: '',
   numOfChildren: '',
   roomId: '',
-  checkInDate: '',
-  checkOutDate: ''
+  checkInDate: null,
+  checkOutDate: null,
 };
 
+
+
+
+
 export const CreateBooking = () => {
-    const handleSubmit = (values) => {
-        console.log('Form Values:', values);
-        
-      };
+  const handleSubmit = (values) => {
+    console.log('Form Values:', values);
+  };
 
   return (
     <div className='flex flex-col'>
@@ -25,101 +32,125 @@ export const CreateBooking = () => {
         Create Booking
       </h1>
       <div className="mt-5">
-      <Formik
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-    >
-      
-        <Form>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Field name="fullName">
-                {({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Full Name"
-                    variant="outlined"
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+        >
+          {({ setFieldValue }) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Field name="fullName">
+                    {({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Full Name"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  </Field>
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <Field name="email">
+                    {({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  </Field>
+                </Grid>
+                <Grid item md={2} xs={12}>
+                  <Field name="roomId">
+                    {({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Room Id"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  </Field>
+                </Grid>
+                <Grid item md={2} xs={12}>
+                  <Field name="numOfAdults">
+                    {({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Num of Adults"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  </Field>
+                </Grid>
+                <Grid item md={2} xs={12}>
+                  <Field name="numOfChildren">
+                    {({ field }) => (
+                      <TextField
+                        {...field}
+                        label="Num of Children"
+                        variant="outlined"
+                        fullWidth
+                      />
+                    )}
+                  </Field>
+                </Grid>
+                <Grid item xs={6} >
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Field name="checkInDate">
+                      {({ field }) => (
+                        <DatePicker
+                          {...field}
+                          label="Check-in Date"
+                          inputFormat="DD/MM/YYYY"
+                          renderInput={(params) => <TextField {...params} fullWidth />}
+                          onChange={(value) => setFieldValue('checkInDate', value)}
+                        />
+                      )}
+                    </Field>
+                    </LocalizationProvider>
+                </Grid>
+                <Grid item xs={6} >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <Field name="checkOutDate">
+                    {({ field }) => (
+                      <DatePicker
+                        {...field}
+                        label="Check-out Date"
+                        inputFormat="DD/MM/YYYY"
+                        renderInput={(params) => <TextField {...params} fullWidth />}
+                        onChange={(value) => setFieldValue('checkOutDate', value)}
+                      />
+                    )}
+                  </Field>
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
                     fullWidth
-                    
-                  />
-                )}
-              </Field>
-            </Grid>
-            <Grid item xs={6}>
-              <Field name="email">
-                {({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                   
-                  />
-                )}
-              </Field>
-            </Grid>
-            <Grid item xs={2}>
-              <Field name="roomId">
-                {({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Room Id"
-                    variant="outlined"
-                    fullWidth
-                   
-                  />
-                )}
-              </Field>
-            </Grid>
-            <Grid item xs={2}>
-              <Field name="numOfAdults">
-                {({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Num of Adults"
-                    variant="outlined"
-                    fullWidth
-                    
-                  />
-                )}
-              </Field>
-            </Grid>
-            <Grid item xs={2}>
-              <Field name="numOfChildren">
-                {({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Num of Children"
-                    variant="outlined"
-                    fullWidth
-                   
-                  />
-                )}
-              </Field>
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-              fullWidth
-                type="submit"
-                variant="contained"
-                color="primary"
-                sx={{
-                  
-                    justifyContent:'center',
-                    backgroundColor: "#FFCF40",
-                    fontWeight: "bold",
-                    '&:hover': {
-                      backgroundColor: "#E5B935",
-                    }
-                  }}
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </Form>
-     
-    </Formik>
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                      justifyContent: 'center',
+                      backgroundColor: "#FFCF40",
+                      fontWeight: "bold",
+                      '&:hover': {
+                        backgroundColor: "#E5B935",
+                      }
+                    }}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
